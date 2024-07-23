f5ae2cfa30a2b4d04ffe580a847a4d5722c2ea23
sumityadav88 <sumityaadav777@gmail.com>
2024-07-23 13:42:06 +0000

    datatype

---
+++ 02-datatype.js
@@ -0,0 +1,27 @@
+/*
+Prefer not to use var
+bacause of issue in block scope and functional scaope
+*/
+
+// 
+
+"use strict"; // treat all JS code as newer version
+
+// alert("hello");
+
+let name="Sumit";
+let age=18;
+let isloggedIn=false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false;
+// null => standalone value;
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+
