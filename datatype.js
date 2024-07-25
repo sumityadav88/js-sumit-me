f18deaecf748e2a40bab021730dc6e81110f9e2a
sumityadav88 <sumityaadav777@gmail.com>
2024-07-25 14:32:43 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,33 @@
+// # primitive
+/*
+7 types: string, Number, Boolean, Null, undefined, 
+symbol, BigInt
+*/
+// const score = 100;
+// const scoreValue = 100.3;
+
+// const isloogedIn = False;
+// const Outsidetemp = null;
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+
+// console.log(id === AnotherId);
+
+//const bigNumber = 33333333333333n;
+
+/*
+Reference(non primitive)
+Array, Objects, Functions
+*/
+const heros = ["Shaktiman", "nagraj", "doga"];
+let myobj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+     console.log("Hello World");
+}
+
+console.log(typeof myFunction);
