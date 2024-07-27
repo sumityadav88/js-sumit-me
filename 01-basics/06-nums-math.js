e339edc8e9436332b3b05ac60d3431b529d0ffb4
sumityadav88 <sumityaadav777@gmail.com>
2024-07-27 15:02:55 +0000

    math

---
+++ 01-basics/06-nums-math.js
@@ -0,0 +1,11 @@
+const score = 480;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernumber = 23.8966;
+console.log(othernumber.toPrecision(3));
