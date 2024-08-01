2e98f4bb4989ec2e850a5c768b39d20ad91ad6d6
sumityadav88 <sumityaadav777@gmail.com>
2024-08-01 16:22:14 +0000

    array

---
+++ 02-basic/01-Array.js
@@ -0,0 +1,15 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+console.log(myArr[0]);
+
+
+const myArr2 = new Array(1, 2, 3, 4);
+console.log(myArr2[1]);
+
+// Array methods
+myArr.push(6);
+console.log(myArr);
+myArr.push(7);
+myArr.pop();
+console.log(myArr);
