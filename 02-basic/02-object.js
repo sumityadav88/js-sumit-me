87549d126de9ba30144d79ee4026330c8dce3d62
sumityadav88 <sumityaadav777@gmail.com>
2024-08-09 06:46:20 +0000

    json api

--- 02-basic/02-object.js
+++ 02-basic/02-object.js
@@ -41,57 +41,93 @@
 
 // obect L-2
 // const tinderUser = new Object() //singelton object
-const tinderUser = {}    //Non singelton object
+// const tinderUser = {}    //Non singelton object
 
-tinderUser.id = "!23abc";
-tinderUser.name = "kelly";
-tinderUser.isLoggedIn = false;
+// tinderUser.id = "!23abc";
+// tinderUser.name = "kelly";
+// tinderUser.isLoggedIn = false;
 
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email: "Kelly@gmail.com",
+//     fullname: {
+//         userFullName: {
+//             firstName: "sumit",
+//             lastName: "yadav"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullname);
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+
+// // const obj3 = {obj1, obj2}
+// // const obj3 = Object.assign({}, obj1, obj2, obj4);
+
+// const obj3 = {...obj1, ...obj2} // spread value
+// console.log(obj3);
+
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     }
+// ]
+
+// users[1].email
 // console.log(tinderUser);
 
-const regularUser = {
-    email: "Kelly@gmail.com",
-    fullname: {
-        userFullName: {
-            firstName: "sumit",
-            lastName: "yadav"
-        }
-    }
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty(`isLogged`));
+
+//Destructuring 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "sumit"
 }
 
-console.log(regularUser.fullname);
+//course.courseInstructor
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj4 = {5: "a", 6: "b"}
+const  {courseInstructor : Instructor }= course;
 
-// const obj3 = {obj1, obj2}
-// const obj3 = Object.assign({}, obj1, obj2, obj4);
+// console.log(courseInstructor);
+console.log(Instructor);
 
-const obj3 = {...obj1, ...obj2} // spread value
-console.log(obj3);
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "hitesh"); destructor in react
+
+// {
+//     "name": "sumit",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
 
 
-const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    }
-]
 
-users[1].email
-console.log(tinderUser);
-
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-
-console.log(tinderUser.hasOwnProperty(`isLogged`));
